7c6e94eb2eeefaa0c294d0bbee07820ec5c8862f
ENDOH takanao <djmchl@gmail.com>
2018-07-18 08:07:07 +0700

    update installer

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 type docker-compose && exit 0
+set -x
 
 # https://docs.docker.com/compose/install/#install-compose
-curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+curl -fL https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
 && chmod +x /usr/local/bin/docker-compose \
 && docker-compose version
